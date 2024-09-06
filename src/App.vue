dcb9d149cf11d421116f3b3406dc9bf9ef4e1827
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-06 15:38:54 +0800

    new file:   .gitignore 	modified:   README.md 	modified:   index.html 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.vue 	new file:   src/assets/vue.svg 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TestDesigner.vue 	new file:   src/main.js 	new file:   src/router/index.js 	new file:   src/style.css 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- router-view :   路由的整个出口, 用来显示和 URL 对应的组件; -->
+    <router-view v-slot="{ Component }">
+        <!-- keep-alive 是Vue提供的一个抽象组件，主要用于保留组件状态或避免重新渲染。 -->
+        <keep-alive>
+            <component :is="Component"/>
+        </keep-alive>
+    </router-view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TestDesigner.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        12311111111111111
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "TestDesigner",
+    components: {},
+    data() {
+        return {}
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
