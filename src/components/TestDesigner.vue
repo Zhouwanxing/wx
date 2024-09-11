09ca0d2c168fe046eba9c4afa219242720771a0f
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-11 14:51:23 +0800

    1

--- src/components/TestDesigner.vue
+++ src/components/TestDesigner.vue
@@ -18,7 +18,10 @@ export default {
     },
     methods: {
         testRoute: function () {
-            this.$router.push({path: '/hello'})
+            // this.$router.push({path: '/hello'})
+            this.$http.get(import.meta.env.VITE_BASE_URL + "/testA").then((response) => {
+                console.log(response.data)
+            })
         }
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
@@ -19,6 +19,33 @@ export default defineConfig({
                 index: path.resolve(__dirname, "./index.html"),
                 zwx: path.resolve(__dirname, "./zwx.html"),
             },
+        }
+    }
+})*/
+
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    console.log(env)
+    return {
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src/')
+            }
+        },
+        build: {
+            minify: false,
+            rollupOptions: {
+                // 配置多个入口文件
+                input: {
+                    // 指定有多个入口html文件
+                    index: path.resolve(__dirname, "./index.html"),
+                    zwx: path.resolve(__dirname, "./zwx.html"),
+                },
+            }
         }
     }
 })
