d228e37769c2ffa5f6bd9ef55400d6c46d5ffccc
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-12 15:59:32 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-/*export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src/')
-        }
-    },
-    build: {
-        minify: false,
-        rollupOptions: {
-            // 配置多个入口文件
-            input: {
-                // 指定有多个入口html文件
-                index: path.resolve(__dirname, "./index.html"),
-                zwx: path.resolve(__dirname, "./zwx.html"),
-            },
-        }
-    }
-})*/
-
 export default defineConfig(({command, mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
-    console.log(env)
+    console.log(env);
     return {
         define: {
             __APP_ENV__: JSON.stringify(env.APP_ENV),
