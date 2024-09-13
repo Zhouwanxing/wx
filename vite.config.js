bcc7fed572633799c03e4db66f6e013cf9a3142b
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-13 16:50:54 +0800

    打包压缩

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import compress from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
@@ -10,7 +11,7 @@ export default defineConfig(({command, mode}) => {
         define: {
             __APP_ENV__: JSON.stringify(env.APP_ENV),
         },
-        plugins: [vue()],
+        plugins: [vue(), compress()],
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src/')
