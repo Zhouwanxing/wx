aebe1212390c41b5b7bf1569ec211c863d226903
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-25 15:44:00 +0800

    增加变量初始化界面

--- src/components/InitData.vue
+++ src/components/InitData.vue
@@ -1,47 +1,87 @@
 <template>
     <div class="init-page">
-        <input v-model="formData.url" placeholder="请输入" style=""/>
-        <div style="margin-top: 20px;">
+        <input v-model="formData.url" placeholder="请输入" style="width: 80%;"/>
+        <div style="margin-top: 10px;">
             <button @click="saveData">保存</button>
         </div>
 
 
-        <div>
-            <input v-model="checkKey" placeholder="请输入" style=""/>
+        <div style="margin-top: 10px;">
+            <input v-model="checkKey" placeholder="请输入" style="width: 80%;"/>
             <div style="margin-top: 20px;">
                 <button @click="getUrls">获取</button>
             </div>
         </div>
-        <div v-for="(item,index) in showUrls" :key="index">
-            {{index}}={{item}}
+        <div style="margin-top: 10px;">
+            <table border="1" cellspacing="0" style="margin-left: auto; margin-right: auto;">
+                <tr>
+                    <td></td>
+                    <td>地址</td>
+                    <td>操作</td>
+                </tr>
+                <tr v-for="(item,index) in showUrls" :key="index">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ item }}</td>
+                    <td>
+                        <button @click="deleteUrl(item)">删除</button>
+                    </td>
+                </tr>
+            </table>
         </div>
+
+        <alert v-if="showAlert" :visible="showAlert" :message="alertMessage" @close="showAlert = false"></alert>
     </div>
 </template>
 
 <script>
+import Alert from "./common/Alert.vue";
+
 export default {
     name: "InitData",
+    components: {Alert},
     data() {
         return {
             formData: {
                 url: ""
             },
             checkKey: "",
-            showUrls: []
+            showUrls: [],
+            showAlert: false,
+            alertMessage: ""
         };
     },
     methods: {
         saveData: function () {
             this.$http.post(import.meta.env.VITE_BASE_URL + "/initData", this.formData).then((response) => {
                 console.log(response);
+                this.alertMessage = "保存成功";
+                this.showAlert = true;
+            }).catch((err) => {
+                console.log(err);
+                this.alertMessage = "保存失败";
+                this.showAlert = true;
             });
         },
         getUrls: function () {
             this.$http.post(import.meta.env.VITE_BASE_URL + "/getUrls", {key: this.checkKey}).then((response) => {
                 console.log(response);
                 this.showUrls = response.data.urls || [];
+            }).catch((err) => {
+                console.log(err);
+                this.alertMessage = "获取失败";
+                this.showAlert = true;
             });
         },
+        deleteUrl: function (url) {
+            this.$http.post(import.meta.env.VITE_BASE_URL + "/deleteUrl", {url: url}).then((response) => {
+                console.log(response);
+                this.showUrls = response.data.urls || [];
+            }).catch((err) => {
+                console.log(err);
+                this.alertMessage = "删除失败";
+                this.showAlert = true;
+            });
+        }
     }
 }
 </script>
---
+++ src/components/common/Alert.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="visible" class="alert-box">
+        <div>
+            {{ message }}
+        </div>
+        <div>
+            <button @click="close">关闭</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['visible', 'message'],
+    methods: {
+        close() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style>
+.alert-box {
+    position: fixed;
+    top: 20%;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 20px;
+    background-color: white;
+    border: 1px solid #ccc;
+    z-index: 1000;
+    border-radius: 10px;
+}
+</style>
