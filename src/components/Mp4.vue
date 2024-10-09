a8a08324625339d7fc3372496e6a46e03752fec8
Zhouwanxing <never_zhouwx@yeah.net>
2024-10-09 15:45:49 +0800

    视频界面调整

--- src/components/Mp4.vue
+++ src/components/Mp4.vue
@@ -1,16 +1,24 @@
 <template>
-    <div class="mp4" style="background-color: #ccc;padding: 5px;">
-        <div style="text-align: center;padding: 10px;color: blue;" @click="page = 0;getList();">刷新</div>
-        <div v-for="(item,index) in list" :key="item._id" class="one-mp4">
-            <div style="padding: 10px;">{{ item.name }}</div>
-            <div class="img-div" @click="clickImg(item)">
-                <img :src="item.img" style="width: 100%;height: 100%;" alt=""/>
+    <div class="mp4" style="background-color: #ccc;padding: 5px; border-radius: 20px;">
+        <div v-show="!selectMp4._id">
+            <div style="text-align: center;padding: 10px;color: blue;" @click="page = 0;list = [];getList();">刷新</div>
+            <div v-for="(item) in list" :key="item._id" class="one-mp4">
+                <div style="padding: 10px;">{{ item.name || item.date }}</div>
+                <div class="img-div" @click="clickImg(item)">
+                    <img :src="item.img" style="width: 100%;height: 100%;" alt=""/>
+                </div>
+                <div style="padding: 10px;">
+                    <button @click="toNotLike(item)">不喜欢</button>
+                </div>
             </div>
-            <div style="padding: 10px;">
-                按钮
-            </div>
+            <div style="text-align: center;padding: 10px;color: blue;" @click="getList">加载更多</div>
         </div>
-        <div style="text-align: center;padding: 10px;color: blue;" @click="getList">加载更多</div>
+        <div v-if="selectMp4._id">
+            <div style="text-align: center;padding: 10px;color: blue;" @click="selectMp4 = {};">关闭</div>
+            <div style="padding: 10px;">{{ selectMp4.name }}</div>
+            <video controls="controls" :src="selectMp4.name ? selectMp4.url : ''"
+                   style="width: 100%;height: 100%;"></video>
+        </div>
     </div>
 </template>
 
@@ -23,7 +31,8 @@ export default {
         return {
             page: 0,
             count: 0,
-            list: []
+            list: [],
+            selectMp4: {},
         }
     },
     mounted() {
@@ -33,8 +42,22 @@ export default {
         }, 1);
     },
     methods: {
+        toNotLike: function (item) {
+            const self = this;
+            Http.sendGet("/mp4/toNotLike?id=" + item._id, function (data) {
+                if (data.error) {
+                    return;
+                }
+                let index = self.list.findIndex(function (item2) {
+                    return item2._id === item._id;
+                });
+                if (index !== -1) {
+                    self.list.splice(index, 1);
+                }
+            });
+        },
         clickImg: function (item) {
-            console.log(item);
+            this.selectMp4 = item;
         },
         getList: function () {
             const self = this;
@@ -67,6 +90,6 @@ export default {
 }
 
 .one-mp4 .img-div {
-    height: 100px;
+    height: 150px;
 }
 </style>
