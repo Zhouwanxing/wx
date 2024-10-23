e2762d1f4ab8c4441641c9f316d520e6a8bfc7a1
Zhouwanxing <never_zhouwx@yeah.net>
2024-10-23 15:42:42 +0800

    refactor: 注释掉 keep-alive 组件
    keep-alive 是 Vue 提供的一个抽象组件，主要用于保留组件状态或避免重新渲染。本次修改将其暂时注释掉，以观察应用的表现。

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
     <!-- router-view :   路由的整个出口, 用来显示和 URL 对应的组件; -->
     <router-view v-slot="{ Component }">
         <!-- keep-alive 是Vue提供的一个抽象组件，主要用于保留组件状态或避免重新渲染。 -->
-        <keep-alive>
+<!--        <keep-alive>-->
             <component :is="Component"/>
-        </keep-alive>
+<!--        </keep-alive>-->
     </router-view>
 </template>
 
