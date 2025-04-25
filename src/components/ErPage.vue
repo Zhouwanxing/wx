4a7fee34587cb7888e452b7f57cdfbd122bb076f
Zhouwanxing <never_zhouwx@yeah.net>
2025-04-25 15:14:25 +0800

    4

--- src/components/ErPage.vue
+++ src/components/ErPage.vue
@@ -8,6 +8,7 @@
                  style="background-color: #ddd;border-radius: 5px;margin: 5px;padding: 5px;">
                 <div :style="item.linkUrl ? 'color:blue;' : ''">{{ item.info }}</div>
                 <div>{{ item.priceStr }}</div>
+                <div>楼栋:{{ areaPlace(item.area) }}</div>
                 <div v-if="(item.histories || []).length > 0" style="margin-top: 10px;color:red;">
                     <div v-for="(one,oneIndex) in item.histories" :key="'a'+oneIndex">
                         <span>{{ one.time }}</span>
@@ -49,8 +50,36 @@ export default {
             Http.sendPost("/tangyue/getAJK", {}, function (res) {
                 self.list = res.data || [];
             });
+        },
+        areaPlace: function (area) {
+            let map = {
+                85: ["A8"],
+                89: ["A8"],
+                91: ["A15", "A16"],
+                112: ["A7"],
+                113: ["A15", "A7", "A16"],
+                114: ["A12"],
+                117: ["A8", "A13", "A7", "A14", "A11"],
+                118: ["A12", "A13", "A7", "A6"],
+                119: ["A13", "A14", "A11"],
+                120: ["A12", "A13", "A7", "A11"],
+                121: ["A7", "A6"],
+                122: ["A10"],
+                123: ["A10"],
+                141: ["A10"],
+                194: ["A9"],
+                159: ["A9"],
+            };
+            let count = -1;
+            while (count++ < 10) {
+                if (map.hasOwnProperty(parseInt(area) + count)) {
+                    return map[parseInt(area) + count].join(",");
+                }
+            }
+            return "";
         }
-    }
+    },
+    filters: {}
 }
 </script>
 
