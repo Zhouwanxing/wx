07fefce11d909ef40e28190e11da7680f56560b1
Zhouwanxing <never_zhouwx@yeah.net>
2025-10-26 21:40:56 +0800

    2

--- src/components/Mp4.vue
+++ src/components/Mp4.vue
@@ -218,6 +218,7 @@ export default {
             }
             axios.get(item.img, {timeout: 3000}).then((response) => {
                 if (response.data) {
+
                     document.getElementById(id).src = response.data;
                 } else {
                     document.getElementById(id).src = item.img;
